af9e214b40f74755eeeba5d0b5f8a1dd9416b839
Mitchell Brooks <mabrooks1@hotmail.co.uk>
2018-07-22 21:18:02 +0100

    Fix JS

--- assets/js/cookie-banner.js
+++ assets/js/cookie-banner.js
@@ -1,21 +1,21 @@
-(function ($) {
+(function( $ ) {
 
-    function bonesCookieBannerCreateCookie(name, value, days) {
+    function bonesCookieBannerCreateCookie( name, value, days ) {
         var expires;
 
-        if (days) {
+        if ( days ) {
             var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
             expires = "; expires=" + date.toGMTString();
         } else {
             expires = "";
         }
-        document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+        document.cookie = encodeURIComponent( name ) + "=" + encodeURIComponent( value ) + expires + "; path=/";
     }
 
-    $(function () {
-        $('#bones-cookie-banner-button').on('click', function () {
-            bonesCookieBannerCreateCookie("cookie-banner-hide-banner", 1, 30);
+    $(function() {
+        $('#bones-cookie-banner-button').on( 'click', function() {
+            bonesCookieBannerCreateCookie( "cookie-banner-hide-banner", 1, 60 );
             $('#bones-cookie-banner').fadeOut();
         });
     });
